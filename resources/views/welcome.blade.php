7bf0eb9c5695044b8a207badf2110ec6967eb389
BlaiseMUBADI <blaisemubadibakajika@gmail.com>
2026-02-09 23:12:39 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Tableau de Bord')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <h5>Bienvenue dans votre gestionnaire bancaire.</h5>
+                <p>Utilisez le menu à gauche pour naviguer.</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
